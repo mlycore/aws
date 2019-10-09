9f7f3750179b4339a850f6b6ab58a7ba055b92cf
maxwell92 <710028463@qq.com>
2019-10-09 18:55:47 +0900

    add test of aws configure

--- kubectl/entrypoint.sh
+++ kubectl/entrypoint.sh
@@ -10,4 +10,6 @@ aws configure set aws_secret_access_key ${AWS_SECRET_ACCESS_KEY}
 echo "$KUBE_CONFIG_DATA" | base64 --decode > /tmp/config
 export KUBECONFIG=/tmp/config
 
-sh -c "kubectl --kubeconfig=$KUBECONFIG $*"
+sh -c "aws configure list"
+
+#sh -c "kubectl --kubeconfig=$KUBECONFIG $*"
