bb094c37186d1feae85c9bef72bb49cd3dd46d29
maxwell92 <710028463@qq.com>
2019-10-09 18:11:51 +0900

    add aws configuration into entrypoint script

--- kubectl/entrypoint.sh
+++ kubectl/entrypoint.sh
@@ -2,6 +2,10 @@
 
 set -e
 
+# Set Aws cli configuration
+aws configure set aws_access_key_id ${AWS_ACCESS_KEY} 
+aws configure set aws_secret_access_key ${AWS_SECRET_ACCESS_KEY}
+
 # Extract the base64 encoded config data and write this to the KUBECONFIG
 echo "$KUBE_CONFIG_DATA" | base64 --decode > /tmp/config
 export KUBECONFIG=/tmp/config
